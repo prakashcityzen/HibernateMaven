3c7268ddbdf35b20db33e0b24aa5186e81c3fadb
Cityzen17 <p.cityzen17@gmail.com>
2018-08-16 14:58:36 +0545

    final commit

--- src/main/java/org/hibernatedemo/mavenhibernate/main/SelectCode.java
+++ src/main/java/org/hibernatedemo/mavenhibernate/main/SelectCode.java
@@ -6,21 +6,24 @@ import org.hibernate.cfg.Configuration;
 import org.hibernatedemo.mavenhibernate.demos.Student;
 import org.hibernatedemo.mavenhibernate.demos.Teacher;
 
+import javax.security.auth.login.Configuration;
 import java.util.List;
+
+import static sun.misc.Version.print;
 
 public class SelectCode {
     public static void main(String[] args) {
-        Configuration configuration= new Configuration();
+        Configuration configuration = new Configuration();
         configuration.configure("hibernate.cfg.xml");
 
-        SessionFactory sessionFactory=configuration.buildSessionFactory();
-        Session session= sessionFactory.openSession();
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
 
-        List<Student> studentList=session.createCriteria(Teacher.class).list();
+        List<Student> studentList = session.createCriteria(Teacher.class).list();
         session.close();
-
-        for(Student s: studentList){
-            System.out.println(s.getId()+" "+s.getName()+" "+s.getAddress()+" "+s.getRoll());
+        print("demo");
+        for (Student s : studentList) {
+            System.out.println(s.getId() + " " + s.getName() + " " + s.getAddress() + " " + s.getRoll());
 
         }
 
